72d7e096474a33fc80205dc69b7f8271fe721c9a
JIYONGFENG <jiyongfeng@163.com>
2025-01-18 01:35:23 +0800

    Add Dockerfile for Streamlit app setup and update OpenAI API settings management
    - Introduced a Dockerfile to streamline the deployment of the Streamlit application, including necessary dependencies and health checks.
    - Updated the settings management in `6_Settings.py` to ensure the configuration file is created if it doesn't exist, enhancing user experience for API key management.

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM python:3.13-slim
+
+WORKDIR /app
+
+# Ensure the installed binary is on the `PATH`
+ENV PATH="/root/.local/bin/:$PATH"
+# 激活虚拟环境
+ENV PATH="/app/.venv/bin/:$PATH"
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    curl \
+    software-properties-common \
+    git \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+
+# Download the latest uv installer
+ADD https://astral.sh/uv/install.sh /uv-installer.sh
+
+# Run the uv installer then remove it
+RUN sh /uv-installer.sh && rm /uv-installer.sh \
+    && uv sync --frozen \
+    && git clone https://github.com/jiyongfeng/llm-examples.git .
+
+EXPOSE 8501
+
+HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=3 \
+    CMD curl -f http://localhost:8501/ || exit 1
+
+ENTRYPOINT ["streamlit", "run", "Chatbot.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- pages/6_Settings.py
+++ pages/6_Settings.py
@@ -5,7 +5,7 @@
 * @Author       : JIYONGFENG jiyongfeng@163.com
 * @Date         : 2025-01-17 15:56:09
 * @Description  :
-* @LastEditTime : 2025-01-17 17:04:08
+* @LastEditTime : 2025-01-18 01:10:27
 * @LastEditors  : JIYONGFENG jiyongfeng@163.com
 * @Copyright (c) 2025 by ZEZEDATA Technology CO, LTD, All Rights Reserved.
 """
@@ -14,29 +14,29 @@ import streamlit as st
 import toml
 from config import settings
 import traceback
+import os
 
 st.title("OpenAI API Settings")
 
-# 从Dynaconf settings中读取OpenAI API Key
-
-openai_api_key = settings.DEEPSEEK_API_KEY
-openai_base_url = settings.DEEPSEEK_BASE_URL
-openai_model = settings.DEEPSEEK_MODEL
-
-input_api_key = st.text_input("OpenAI API Key", value=openai_api_key)
-input_base_url = st.text_input("OpenAI Base URL", value=openai_base_url)
-input_model = st.text_input("OpenAI Model", value=openai_model)
+# user input
+openai_api_key = st.text_input("OpenAI API Key", value=settings.DEEPSEEK_API_KEY)
+openai_base_url = st.text_input("OpenAI Base URL", value=settings.DEEPSEEK_BASE_URL)
+openai_model = st.text_input("OpenAI Model", value=settings.DEEPSEEK_MODEL)
 
 if st.button("Save"):
     # save settings to /config/.secrets.toml
     try:
+        # check if config/.secrets.toml exists
+        if not os.path.exists("config/.secrets.toml"):
+            with open("config/.secrets.toml", "w") as f:
+                toml.dump({}, f)
         with open("config/.secrets.toml", "r") as f:
             config = toml.load(f)
         with open("config/.secrets.toml", "w") as f:
             config["default"] = {
-                "DEEPSEEK_API_KEY": input_api_key,
-                "DEEPSEEK_BASE_URL": input_base_url,
-                "DEEPSEEK_MODEL": input_model,
+                "DEEPSEEK_API_KEY": openai_api_key,
+                "DEEPSEEK_BASE_URL": openai_base_url,
+                "DEEPSEEK_MODEL": openai_model,
             }
             toml.dump(config, f)
             st.success("Settings saved successfully")
